8baecfcef9593e3ced9528be00ec90d7ce32b11f
Chen GuangChong <18889357323@163.com>
2022-03-23 16:42:08 +0800

    lib

---
+++ lib_base/src/main/java/com/common/mh/lib_base/TestUtil.java
@@ -0,0 +1,12 @@
+package com.common.mh.lib_base;
+
+/**
+ * @AUTHOR Shige Chen
+ * @DESCRIPTION TODO
+ * @CREATETIME 2022年03月23日 16:38:00
+ */
+public class TestUtil {
+    public static String getTestString() {
+        return "getTestString";
+    }
+}
